bdedbd3399da763581ff08844ac8caea88277640
dns-sec <dennis.a.sys@gmail.com>
2025-12-09 10:59:26 +0100

    Ovningsskript 1a

---
+++ hax.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Detta script samlar in systeminformation - RECON
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Frans Schartau
+# Last Update: 2025-01-01
+
+
+echo
+echo "=== SYSTEMINFO ==="
+uname -a
+
+echo
+echo "=== AKTUELL ANVÄNDARE ==="
+echo $USER
+
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+
+echo
+echo "Distribution:"
+echo
+cat /etc/os-release | grep -E '^(NAME|VERSION)='
+echo -n "Uptime:  "
+uptime -p
+echo -n "Datum & tid:  "
+date
+echo
+
